70317af5c91b8eaca2d6b95e98a98840a17c63d8
Frank McDermott <fmcdermott4@gmail.com>
2021-06-17 08:38:07 -0400

    LPN route created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const lpnRoutes = require("./lpn-routes");
 
 router.use("/users", userRoutes);
+router.use("/lpn", lpnRoutes);
 
 module.exports = router;
---
+++ controllers/api/lpn-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const { AmazonItem } = require("../../models");
+
+// find Amazon Item Data
+router.post("/lpn", async(req, res) => {
+    try {
+        const dbLpnData = await AmazonItem.findOne({
+            where: {
+                lpn: req.body.lpn,
+            },
+        });
+        if (!dbLpnData) {
+            res
+                .status(400)
+                .json({ message: "Incorrect LPN. Please try again!" });
+            return;
+        }
+        res.status(200).json({ data: dbLpnData, message: "You have the LPN data!" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
